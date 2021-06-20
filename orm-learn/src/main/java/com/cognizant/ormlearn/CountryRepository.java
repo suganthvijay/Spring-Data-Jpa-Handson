56a105dae7b291829310b5556f84b070a9c40af1
suganth vijayakumar <61311051+suganthvijay@users.noreply.github.com>
2021-06-20 23:52:09 +0530

    Delete orm-learn directory

--- orm-learn/src/main/java/com/cognizant/ormlearn/CountryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cognizant.ormlearn;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.cognizant.ormlearn.model.country;
-@Repository
-public interface CountryRepository extends JpaRepository<country,String> {
-}
-
