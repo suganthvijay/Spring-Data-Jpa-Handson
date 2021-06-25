4b4c49ac786b641b6083f9091315064ab4d4699f
suganth vijayakumar <61311051+suganthvijay@users.noreply.github.com>
2021-06-25 18:01:44 +0530

    Add files via upload

---
+++ Day 2 Session-2/QuizHQL-1/src/main/java/com/cognizant/ormlearn/repository/OptionsRepository.java
@@ -0,0 +1,10 @@
+  
+package com.cognizant.ormlearn.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cognizant.ormlearn.model.Options;
+
+public interface OptionsRepository extends JpaRepository<Options, String>{
+
+}
